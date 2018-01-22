9c32dabc6684be9ffb8e787159d09e457a09e461
Riaan Potgieter <riaan.potgieter@standardbank.co.za>
2018-01-22 17:39:00 +0200

    commander change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ program
   .option('-e, --enable <HID>', 'disable <HIDs>', list)
   .option('-s, --status <HID>', 'status of <HIDs>', list)
   .option('-r, --restart <HID>', 'restart <HIDs>', list)
-  .option('-c, --cron <command> <HID>', 'create cron with <command> and <HIDs>', list)
+  .option('-c, --cron <command> <HID> [CRON]', 'create cron restarter with <command> and <HIDs> [CRON] default is 8 hours', list)
 
 program.parse(process.argv)
 operations.run(program)
